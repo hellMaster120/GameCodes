82d04cfcddb5cccb0f8f4d5e21152df4649980df
HellMaster120 <51432361+hellMaster120@users.noreply.github.com>
2020-09-04 19:40:21 -0400

    Update htmlcode.js

--- htmlcode.js
+++ htmlcode.js
@@ -1,3 +1,15 @@
+
+function LoadPassWords(){}
+function LoadEmailes(){
+ Pass = "ewrwe"
+}
+var Code = `
+  console.log(Pass)
+`
+function LoadHtml(){
+  
+}
+function CheckEmail(){}
 document.head.innerHTML = ""
 document.body.innerHTML = ""
 var Body = document.body
@@ -5,5 +17,7 @@ var Head = document.head
 var attScript = document.createAttribute("id")
 var LoadScript = document.createElement("script")
 attScript.value = "Main Script"
+LoadEmailes()
+LoadScript.innerHTML = Code
 Body.appendChild(LoadScript)
 LoadScript.setAttributeNode(attScript)
